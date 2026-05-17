c381b63270422ce94b857bb01711ca026a4b9acb
Bibber <thomassebas.derksen@gmail.com>
2026-05-17 22:07:49 +0200

    ID

--- system_identification/pendulum_rod_2_model_coulomb.m
+++ system_identification/pendulum_rod_2_model_coulomb.m
@@ -1,4 +1,5 @@
-function [dx, y] = pendulum_rod_2_model_coulomb(t, x, u, p_b2, p_g2, p_c2, varargin)
+function [dx, y] = pendulum_rod_2_model_coulomb( ...
+    t, x, u, p_b2, p_g2, p_c2, eps_v, varargin)
 %PENDULUM_ROD_2_MODEL_COULOMB Nonlinear passive-link model for idnlgrey.
 %
 % Model:
@@ -13,34 +14,29 @@ function [dx, y] = pendulum_rod_2_model_coulomb(t, x, u, p_b2, p_g2, p_c2, varar
 %   x(2) = theta_dot  [rad/s]
 %
 % Parameters:
-%   p_b2 = viscous damping parameter        [1/s]
-%   p_g2 = gravity parameter                [1/s^2]
-%   p_c2 = smooth Coulomb friction strength [rad/s^2]
+%   p_b2  = viscous damping parameter         [1/s]
+%   p_g2  = gravity parameter                 [1/s^2]
+%   p_c2  = smooth Coulomb friction strength  [rad/s^2]
+%   eps_v = smoothing velocity                [rad/s]
 %
 % Input:
 %   u is included for idnlgrey compatibility, but is not used.
-%
-% Notes:
-%   theta is assumed to be the angle deviation from the stable hanging
-%   equilibrium. Therefore the gravitational term is -p_g2*sin(theta).
-%
-%   tanh(theta_dot/eps_v) is used instead of sign(theta_dot) because sign()
-%   is discontinuous and can make nonlinear grey-box estimation difficult.
 
 theta     = x(1);
 theta_dot = x(2);
-
-% Smooth transition velocity for Coulomb friction.
-% Smaller values approximate sign(theta_dot) more closely.
-% Larger values make the model smoother and easier to estimate.
-eps_v = 0.01;  % [rad/s]
 
 dx = zeros(2,1);
 
 dx(1) = theta_dot;
 
-dx(2) = -p_b2*theta_dot ...
-        -p_c2*tanh(theta_dot/eps_v) ...
+% Numerical safety
+eps_v = max(eps_v, 1e-8);
+
+viscous_friction = p_b2 * theta_dot;
+coulomb_friction = p_c2 * tanh(theta_dot / eps_v);
+
+dx(2) = -viscous_friction ...
+        -coulomb_friction ...
         -p_g2*sin(theta);
 
 y = theta;
