e0bf91d6297cb6ce289d3dced9c2fa51987405a2
Bibber <thomassebas.derksen@gmail.com>
2026-06-11 22:13:43 +0200

    EKf -> RK4

--- extended_kalman_filter/rotpendulumEkfStateTransition.m
+++ extended_kalman_filter/rotpendulumEkfStateTransition.m
@@ -1,6 +1,12 @@
 function x_k_1 = rotpendulumEkfStateTransition(x_k, u_k, Ts, p)
 
-    [x_dot, ~] = nonlinearPlant(x_k, u_k, p);
-    x_k_1 = x_k + x_dot * Ts;
+    x_k = x_k(:);
+
+    [k1, ~] = nonlinearPlant(x_k, u_k, p);
+    [k2, ~] = nonlinearPlant(x_k + 0.5 * Ts * k1, u_k, p);
+    [k3, ~] = nonlinearPlant(x_k + 0.5 * Ts * k2, u_k, p);
+    [k4, ~] = nonlinearPlant(x_k + Ts * k3, u_k, p);
+
+    x_k_1 = x_k + (Ts / 6) * (k1 + 2*k2 + 2*k3 + k4);
 
 end
