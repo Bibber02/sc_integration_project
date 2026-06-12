3f174a43b95b55daf2cf98a0a28af110de43724b
Bibber <thomassebas.derksen@gmail.com>
2026-06-12 14:11:03 +0200

    wrapper

--- extended_kalman_filter/rotpendulumEkfStateTransition.m
+++ extended_kalman_filter/rotpendulumEkfStateTransition.m
@@ -1,12 +1,23 @@
 function x_k_1 = rotpendulumEkfStateTransition(x_k, u_k, Ts, p)
 
-    x_k = x_k(:);
+    x_k = double(x_k(:));
+    u_k = double(u_k);
+    Ts  = double(Ts);
+    p   = double(p(:));
 
     [k1, ~] = nonlinearPlant(x_k, u_k, p);
+    k1 = double(k1(:));
+
     [k2, ~] = nonlinearPlant(x_k + 0.5 * Ts * k1, u_k, p);
+    k2 = double(k2(:));
+
     [k3, ~] = nonlinearPlant(x_k + 0.5 * Ts * k2, u_k, p);
+    k3 = double(k3(:));
+
     [k4, ~] = nonlinearPlant(x_k + Ts * k3, u_k, p);
+    k4 = double(k4(:));
 
     x_k_1 = x_k + (Ts / 6) * (k1 + 2*k2 + 2*k3 + k4);
+    x_k_1 = double(x_k_1(:));
 
 end
--- extended_kalman_filter/rotpendulumEkfStateTransitionMuxed.m
+++ extended_kalman_filter/rotpendulumEkfStateTransitionMuxed.m
@@ -1,11 +1,13 @@
 function x_k_1 = rotpendulumEkfStateTransitionMuxed(x_k, ekfInput)
 
-    ekfInput = ekfInput(:);
+    x_k = double(x_k(:));
+    ekfInput = double(ekfInput(:));
 
     u_k = ekfInput(1);
     Ts  = ekfInput(2);
     p   = ekfInput(3:end);
 
     x_k_1 = rotpendulumEkfStateTransition(x_k, u_k, Ts, p);
+    x_k_1 = double(x_k_1(:));
 
 end
